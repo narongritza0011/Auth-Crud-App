5b0d1063466d4bd27fb7d5a0124497adfc50f9db
narongrit <narongrit.ku@psru.ac.th>
2023-06-02 12:54:06 +0700

    first commit

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,82 @@
+import axios from "axios";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [msg, setMsg] = useState("");
+  const navigate = useNavigate();
+
+  const Auth = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:5000/login", {
+        email: email,
+        password: password,
+      });
+      console.log(response.data);
+      navigate("/dashboard");
+    } catch (error) {
+      if (error.response) {
+        setMsg(error.response.data.msg);
+      }
+    }
+  };
+
+  return (
+    <section className="hero has-background-grey-light is-fullheight is-fullwidth">
+      <div className="hero-body">
+        <div className="container">
+          <div className="columns is-centered">
+            <div className="column is-4-desktop">
+              <form className="box" onSubmit={Auth}>
+                <p className="has-text-centered has-text-danger">{msg}</p>
+                <div className="field mt-5">
+                  <label className="label">Email or Username</label>
+                  <div className="controls">
+                    <input
+                      type="text"
+                      className="input"
+                      placeholder="Username"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="field mt-5">
+                  <label className="label">Password</label>
+                  <div className="controls">
+                    <input
+                      type="password"
+                      className="input"
+                      placeholder="******"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="field mt-5">
+                  <button className="button is-success is-fullwidth">
+                    Login
+                  </button>
+                </div>
+                <div className="field mt-5">
+                  <Link
+                    to="/register"
+                    className="button is-warning  is-fullwidth"
+                  >
+                    Register
+                  </Link>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,66 @@
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+const Navbar = () => {
+  const navigate = useNavigate();
+  const Logout = async () => {
+    try {
+      await axios.delete("http://localhost:5000/logout");
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <nav
+      className="navbar is-light"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <Link className="navbar-item" to="/dashboard">
+            <img
+              src="https://bulma.io/images/bulma-logo.png"
+              width="112"
+              height="28"
+            />
+          </Link>
+
+          <Link
+            role="button"
+            className="navbar-burger burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </Link>
+        </div>
+
+        <div id="navbarBasicExample" className="navbar-menu">
+          <div className="navbar-start">
+            <Link to="/dashboard" className="navbar-item">
+              Dashboard
+            </Link>
+          </div>
+
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="buttons">
+                <button onClick={Logout} className="button is-light">
+                  Log Out
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
